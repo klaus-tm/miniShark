cdf63477e6f5fbcdf4cb200d08bd2827f264ee7e
ClaudiuCercel <46817410+klaus-tm@users.noreply.github.com>
2023-05-11 00:02:44 +0300

    adding vector of packets and reading capability of whole file with data

--- backend/PcapRead.cpp
+++ backend/PcapRead.cpp
@@ -4,7 +4,8 @@
 #include <string>
 #include <vector>
 #include <winsock.h>
-
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
@@ -25,6 +26,11 @@ struct pcap_packet_header {
     uint32_t orig_len; // actual length of packet
 };
 
+struct pcap_packet {
+    pcap_packet_header pcapPacketHeader;
+    vector<uint8_t> data;
+};
+
 int main() {
     string filePath;
     // Open the PCAP file
@@ -37,41 +43,64 @@ int main() {
     // Read the PCAP file header
     pcap_global_header file_header;
     file.read(reinterpret_cast<char*>(&file_header), sizeof(file_header)); //reads pcap file header and interprets it
-    //printf("%x", ntohs(file_header.version_major));
+    file_header.magic_number = ntohl(file_header.magic_number);//ntohl
+    file_header.version_major = ntohs(file_header.version_major);
+    file_header.version_minor = ntohs(file_header.version_minor);
+    file_header.thiszone = ntohl(file_header.thiszone);//
+    file_header.sigfigs = ntohl(file_header.sigfigs);//
+    file_header.snaplen = ntohl(file_header.snaplen);//
+    file_header.network = ntohl(file_header.network);//
 
-    cout << "Magic number: " <<hex<< file_header.magic_number<< "\n";
-    cout << "Version major: " <<file_header.version_major<< "\n";
-    cout << "Version minor: " <<file_header.version_minor<< "\n";
-    cout << "Thiszone: " <<file_header.thiszone<< "\n";
-    cout << "Sigfigs: " <<file_header.sigfigs<< "\n";
-    cout << "Snaplen: " <<file_header.snaplen<< "\n";
-    cout << "Network: " <<file_header.network<< "\n";
+    stringstream ss;
+    ss << hex << file_header.magic_number;
+    if (ss.str() == "a1b2c3d4")
+        cout << "ENDIANESS: BIG ENDIAN\n";
+    else cout << "ENDIANESS: LITTLE ENDIAN\n";
+    cout << "VERSION: " << file_header.version_major << "." << file_header.version_minor << "\n";
+    cout << "Thiszone(not important): " << file_header.thiszone << "\n";
+    cout << "Sigfigs(not important): " << file_header.sigfigs << "\n";
+    cout << "MAX BYTES ALLOWED IN PACKETS: " << file_header.snaplen << " bytes\n";
+    if (file_header.network == 1)
+        cout << "NETWORK: ETHERNET\n";
+    else if (file_header.network == 105)
+        cout << "NETWORK: IEEE 802.11 wireless LAN\n";
+    else if (file_header.network == 9)
+        cout << "NETWORK: PPP(Point-to-point-protocol)\n";
+    else if (file_header.network == 104)
+        cout << "NETWORK: HDLC(High-Level Data Link Control)\n";
+    else cout << "NETWORK: FDDI(Fiber Distributed Data Interface)\n";
+    cout << "\n\n\n";
 
     // Loop through the packets in the file
-    int packet_count = 0;
-    while (file.peek() != EOF) {
-        // Read the packet header
-        pcap_packet_header packet_header;
-        file.read(reinterpret_cast<char*>(&packet_header), sizeof(packet_header));
-
-        // Read the packet data
-        char* packet_data = new char[packet_header.incl_len];
-        file.read(packet_data, packet_header.incl_len);
-
-        // Do something with the packet
-        cout << "Packet #" << ++packet_count << ": \n";
-        cout << "Timestamp seconds: " << packet_header.ts_sec << "\n";
-        cout << "Timestamp microseconds/nanoseconds: " << packet_header.ts_usec << "\n";
-        cout << "Number of octets saved into the capture: " << packet_header.incl_len << "\n";
-        cout << "Actual packet size: " << packet_header.orig_len << "\n";
-
-        // Free the packet data buffer
-        delete[] packet_data;
+    vector<pcap_packet>packets;
+    pcap_packet_header packetHeader;
+    while (file.read(reinterpret_cast<char*>(&packetHeader), sizeof(packetHeader))) {
+        pcap_packet pcapPacket;
+        pcapPacket.pcapPacketHeader = packetHeader;
+        pcapPacket.data.resize(ntohl(packetHeader.incl_len));
+        file.read(reinterpret_cast<char*>(pcapPacket.data.data()), ntohl(packetHeader.incl_len));
+        packets.push_back(pcapPacket);
     }
-
     // Close the PCAP file
     file.close();
 
+    for (int i = 0; i < packets.size(); ++i) {
+        pcap_packet packet = packets[i];
+        cout << "Packet " << i+1 << "\n";
+        packet.pcapPacketHeader.ts_sec = ntohl(packet.pcapPacketHeader.ts_sec);
+        packet.pcapPacketHeader.ts_usec = ntohl(packet.pcapPacketHeader.ts_usec);
+        packet.pcapPacketHeader.incl_len = ntohl(packet.pcapPacketHeader.incl_len);
+        packet.pcapPacketHeader.orig_len = ntohl(packet.pcapPacketHeader.orig_len);
+
+        cout << "TIMESTAMP: " << packet.pcapPacketHeader.ts_sec << "." << packet.pcapPacketHeader.ts_usec << "\n";
+        cout << "CAPTURED LENGTH: " << packet.pcapPacketHeader.incl_len << " bytes\n";
+        cout << "ORIGINAL LENGTH: " << packet.pcapPacketHeader.orig_len << " bytes\n";
+        for (::uint8_t data:packet.data) {
+            cout << hex << data;
+        }
+        cout << "\n\n\n";
+    }
+    //print packets
     return 0;
 
 }
