1cc322b900a70338e6dfbdbc589d5adc59e227b3
ClaudiuCercel <46817410+klaus-tm@users.noreply.github.com>
2023-05-13 13:23:54 +0300

    fixed pain

--- backend/PcapRead.cpp
+++ backend/PcapRead.cpp
@@ -10,10 +10,11 @@
 
 using namespace std;
 uint32_t value = 0x12345678;
+bool littleEndian = false;
 struct PcapGlobalHeader {
-    uint32_t magic_number;  /* magic number */
-    uint16_t version_major; /* major version number */
-    uint16_t version_minor; /* minor version number */
+    uint32_t magicNumber;  /* magic number */
+    uint16_t versionMajor; /* major version number */
+    uint16_t versionMinor; /* minor version number */
     int32_t  thiszone;      /* GMT to local correction */
     uint32_t sigfigs;       /* accuracy of timestamps */
     uint32_t snaplen;       /* max length of captured packets, in octets */
@@ -21,10 +22,10 @@ struct PcapGlobalHeader {
 };
 
 struct PcapPacketHeader {
-    uint32_t ts_sec;   // timestamp seconds
-    uint32_t ts_usec;  // timestamp microseconds/nanoseconds
-    uint32_t incl_len; // number of octets of packet saved in file
-    uint32_t orig_len; // actual length of packet
+    uint32_t tsSec;   // timestamp seconds
+    uint32_t tsUsec;  // timestamp microseconds/nanoseconds
+    uint32_t inclLen; // number of octets of packet saved in file
+    uint32_t origLen; // actual length of packet
 };
 
 struct PcapPacket {
@@ -51,6 +52,16 @@ struct IPHeader {
     uint32_t destinationIP;     // Destination IP address
 };
 
+void checkEndianess(uint32_t magicNumber);
+
+void printFileHeader(PcapGlobalHeader fileHeader);
+
+void printPacket(vector<PcapPacket> packets);
+
+void parseEthernet(vector<uint8_t> packetData);
+
+void parseIPv4(IPHeader ipHeader, vector<uint8_t> packetData);
+
 int main() {
     string filePath;
     WSADATA wsaData;
@@ -58,51 +69,30 @@ int main() {
         std::cerr << "Failed to initialize Winsock." << std::endl;
         return 1;
     }
+
     // Open the PCAP file fuzz-2006-07-09-6023
     ifstream file("C:\\Users\\cerce\\Desktop\\fuzz-2006-07-09-6023.pcap", ios::binary);
     if (!file.is_open()) {
         cerr << "Error opening pcap file" << endl;
         return 1;
     }
-
-    // Read the PCAP file header
-    PcapGlobalHeader file_header;
-    file.read(reinterpret_cast<char*>(&file_header), sizeof(file_header)); //reads pcap file header and interprets it
-//    file_header.magic_number = ntohl(file_header.magic_number);//ntohl
-//    file_header.version_major = ntohs(file_header.version_major);
-//    file_header.version_minor = ntohs(file_header.version_minor);
-//    file_header.thiszone = ntohl(file_header.thiszone);//
-//    file_header.sigfigs = ntohl(file_header.sigfigs);//
-//    file_header.snaplen = ntohl(file_header.snaplen);//
-//    file_header.network = ntohl(file_header.network);//
+    //check if system is either big or little endian
     uint32_t networkValue = htonl(value);
 
-    if (value == networkValue) {
-        std::cout << "System is using big-endian byte order." << std::endl;
-    } else {
-        std::cout << "System is using little-endian byte order." << std::endl;
-    }
+    if (value == networkValue)
+        cout << "System is using big-endian byte order.\n";
+    else
+        cout << "System is using little-endian byte order.\n";
 
-    WSACleanup();
-    stringstream ss;
-    ss << hex << file_header.magic_number;
-    if (ss.str() == "a1b2c3d4")
-        cout << "ENDIANESS: BIG ENDIAN\n";
-    else cout << "ENDIANESS: LITTLE ENDIAN\n";
-    cout << "VERSION: " << file_header.version_major << "." << file_header.version_minor << "\n";
-    cout << "Thiszone(not important): " << file_header.thiszone << "\n";
-    cout << "Sigfigs(not important): " << file_header.sigfigs << "\n";
-    cout << "MAX BYTES ALLOWED IN PACKETS: " << file_header.snaplen << " bytes\n";
-    if (file_header.network == 1)
-        cout << "NETWORK: ETHERNET\n";
-    else if (file_header.network == 105)
-        cout << "NETWORK: IEEE 802.11 wireless LAN\n";
-    else if (file_header.network == 9)
-        cout << "NETWORK: PPP(Point-to-point-protocol)\n";
-    else if (file_header.network == 104)
-        cout << "NETWORK: HDLC(High-Level Data Link Control)\n";
-    else cout << "NETWORK: FDDI(Fiber Distributed Data Interface)\n";
-    cout << "\n\n";
+    // Read the PCAP file header
+    PcapGlobalHeader fileHeader;
+    file.read(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+
+    //check for the endianess of the file
+    checkEndianess(fileHeader.magicNumber);
+
+    //print the rest of the pcap header
+    printFileHeader(fileHeader);
 
     // Loop through the packets in the file
     vector<PcapPacket>packets;
@@ -110,43 +100,91 @@ int main() {
     while (file.read(reinterpret_cast<char*>(&packetHeader), sizeof(packetHeader))) {
         PcapPacket pcapPacket;
         pcapPacket.pcapPacketHeader = packetHeader;
-        pcapPacket.data.resize(ntohl(packetHeader.incl_len));
-        file.read(reinterpret_cast<char*>(pcapPacket.data.data()), ntohl(packetHeader.incl_len));
+        if (littleEndian == true){
+            pcapPacket.data.resize(ntohl(packetHeader.inclLen));
+            file.read(reinterpret_cast<char*>(pcapPacket.data.data()), ntohl(packetHeader.inclLen));
+        }
+        else{
+            pcapPacket.data.resize(packetHeader.inclLen);
+            file.read(reinterpret_cast<char*>(pcapPacket.data.data()), packetHeader.inclLen);
+        }
         packets.push_back(pcapPacket);
     }
     // Close the PCAP file
     file.close();
+    //print the packets captured
+    printPacket(packets);
 
-    for (int i = 0; i < packets.size(); ++i) {
-        PcapPacket packet = packets[i];
-        cout << "Packet " << i+1 << "\n";
-        packet.pcapPacketHeader.ts_sec = ntohl(packet.pcapPacketHeader.ts_sec);
-        packet.pcapPacketHeader.ts_usec = ntohl(packet.pcapPacketHeader.ts_usec);
-        packet.pcapPacketHeader.incl_len = ntohl(packet.pcapPacketHeader.incl_len);
-        packet.pcapPacketHeader.orig_len = ntohl(packet.pcapPacketHeader.orig_len);
-
-        cout << "TIMESTAMP: " << packet.pcapPacketHeader.ts_sec << "." << packet.pcapPacketHeader.ts_usec << "\n";
-        cout << "CAPTURED LENGTH: " << packet.pcapPacketHeader.incl_len << " bytes\n";
-        cout << "ORIGINAL LENGTH: " << packet.pcapPacketHeader.orig_len << " bytes\n";
-        for (size_t i = 0; i < packet.data.size(); ++i) {
-            cout << packet.data[i];
-
-            if ((i + 1) % 16 == 0)
-                cout << "\n";
-        }
-        cout << "\n\n";
-    }
-    //print packets
+    WSACleanup();
     return 0;
 
 }
+
+void checkEndianess(uint32_t magicNumber) {
+    stringstream ss;
+    ss << hex << magicNumber;
+    if (ss.str() == "a1b2c3d4")
+        cout << "ENDIANESS: BIG ENDIAN\n";
+    else{
+        cout << "ENDIANESS: LITTLE ENDIAN\n";
+        littleEndian = true;
+    }
+}
+
+void printFileHeader(PcapGlobalHeader fileHeader) {
+    if (littleEndian == true){
+        fileHeader.versionMajor = ntohs(fileHeader.versionMajor);
+        fileHeader.versionMinor = ntohs(fileHeader.versionMinor);
+        fileHeader.thiszone = ntohl(fileHeader.thiszone);//
+        fileHeader.sigfigs = ntohl(fileHeader.sigfigs);//
+        fileHeader.snaplen = ntohl(fileHeader.snaplen);//
+        fileHeader.network = ntohl(fileHeader.network);//
+    }
+
+    cout << "VERSION: " << fileHeader.versionMajor << "." << fileHeader.versionMinor << "\n";
+    cout << "Thiszone(not important): " << fileHeader.thiszone << "\n";
+    cout << "Sigfigs(not important): " << fileHeader.sigfigs << "\n";
+    cout << "MAX BYTES ALLOWED IN PACKETS: " << fileHeader.snaplen << " bytes\n";
+    if (fileHeader.network == 1)
+        cout << "NETWORK: ETHERNET\n";
+    else if (fileHeader.network == 105)
+        cout << "NETWORK: IEEE 802.11 wireless LAN\n";
+    else if (fileHeader.network == 9)
+        cout << "NETWORK: PPP(Point-to-point-protocol)\n";
+    else if (fileHeader.network == 104)
+        cout << "NETWORK: HDLC(High-Level Data Link Control)\n";
+    else cout << "NETWORK: FDDI(Fiber Distributed Data Interface)\n";
+    cout << "\n\n";
+}
+
+void printPacket(vector<PcapPacket> packets) {
+    for (int i = 0; i < packets.size(); ++i) {
+        PcapPacket packet = packets[i];
+        cout << "Packet " << i+1 << "\n";
+
+        if (littleEndian == true){
+            packet.pcapPacketHeader.tsSec = ntohl(packet.pcapPacketHeader.tsSec);
+            packet.pcapPacketHeader.tsUsec = ntohl(packet.pcapPacketHeader.tsUsec);
+            packet.pcapPacketHeader.inclLen = ntohl(packet.pcapPacketHeader.inclLen);
+            packet.pcapPacketHeader.origLen = ntohl(packet.pcapPacketHeader.origLen);
+        }
+
+        cout << "TIMESTAMP: " << packet.pcapPacketHeader.tsSec << "." << packet.pcapPacketHeader.tsUsec << "\n";
+        cout << "CAPTURED LENGTH: " << packet.pcapPacketHeader.inclLen << " bytes\n";
+        cout << "ORIGINAL LENGTH: " << packet.pcapPacketHeader.origLen << " bytes\n";
+
+        parseEthernet(packet.data);
+
+        cout << "\n\n";
+    }
+}
+
 void parseEthernet(vector<uint8_t> packetData) {
     EthernetHeader ethernetHeader;
     memcpy(&ethernetHeader, packetData.data(), sizeof(EthernetHeader));
 
     //byte order reversing for ethernet header
-    //byte order for destinationMac and printing
-    reverse(begin(ethernetHeader.destinationMAC), end(ethernetHeader.destinationMAC));
+    //destinationMac printing
     cout << "DESTINATION MAC ADRESS: " << hex << setfill('0');
     for (int i = 0; i < 6; ++i) {
         cout << setw(2) << static_cast<int>(ethernetHeader.destinationMAC[i]);
@@ -155,8 +193,7 @@ void parseEthernet(vector<uint8_t> packetData) {
     }
     cout << dec << "\n";
 
-    //byte order for sourceMac and printing
-    reverse(begin(ethernetHeader.sourceMAC), end(ethernetHeader.sourceMAC));
+    //sourceMac printing
     cout << "SOURCE MAC ADRESS: " << hex << setfill('0');
     for (int i = 0; i < 6; ++i) {
         cout << setw(2) << static_cast<int>(ethernetHeader.sourceMAC[i]);
@@ -172,107 +209,8 @@ void parseEthernet(vector<uint8_t> packetData) {
         IPHeader ipHeader;
         memcpy(&ipHeader, packetData.data() + sizeof(EthernetHeader), sizeof(IPHeader));
 
-        //version and internet header length (ihl)
-        uint8_t version = (ipHeader.versionIHL >> 4) & 0x0F;
-        cout << "VERSION: " << static_cast<unsigned int>(version) << "\n";
-        uint8_t ihl = (ipHeader.versionIHL & 0x0F) * 4;
-        cout << "INTERNET HEADER LENGTH(IHL): " << static_cast<unsigned int>(ihl) <<"\n";
+        parseIPv4(ipHeader, packetData);
 
-        //type of service and Differentiated Service Code Point(DSCP)
-        cout << "TYPE OF SERVICE: ";
-        uint8_t dscp = (ipHeader.typeOfService >> 2) & 0x3F;
-        if (static_cast<unsigned int>(dscp) == 0)
-            cout << "Default\n";
-        else if (static_cast<unsigned int>(dscp) == 46)
-            cout << "Expedited Forwarding\n";
-        else if (dscp == 0x08 or dscp == 0x0C or dscp == 0x10 or dscp == 0x10 or dscp == 0x1C or dscp == 0x20 or dscp == 0x28 or dscp == 0x2C or dscp == 0x30 or dscp == 0x38 or dscp == 0x3C or dscp == 0x40)
-            cout << "Assured Forwarding\n";
-        else cout << "Unknown\n";
-
-        //total length
-        ipHeader.totalLength = ntohs(ipHeader.totalLength);
-        cout << "TOTAL LENGTH: " << ipHeader.totalLength << " bytes\n";
-
-        //identification field
-        ipHeader.identification = ntohs(ipHeader.identification);
-        cout << "IDENTIFICATION: " << ipHeader.identification << "\n";
-
-        //flags and fragment offset
-        ipHeader.flagsFragmentOffset = ntohs(ipHeader.flagsFragmentOffset);
-        uint16_t flag = (ipHeader.flagsFragmentOffset & 0xE000) >> 13;
-        uint16_t fragmentOffset = ipHeader.flagsFragmentOffset & 0x1FFF;
-        bool isDFSet = (flag & 0x4000) != 0;
-        bool isMFSet = (flag & 0x2000) != 0;
-
-        cout << "FLAG: ";
-        if (isDFSet)
-            cout << "Don't Fragment (DF)\n";
-        else if (isMFSet)
-            cout << "More Fragments (MF)\n";
-        else if (flag == 0)
-            cout << "Reserved\n";
-        else cout << "Unknown\n";
-        cout << "FRAGMENT OFFSET: " << fragmentOffset << "\n";
-
-        //time to live
-        cout << "TIME TO LIVE: " << static_cast<int>(ipHeader.timeToLive) << "\n";
-
-        //protocol
-        cout << "PROTOCOL: ";
-        if(static_cast<int>(ipHeader.protocol) == 1){
-            cout << "ICMP //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 6){
-            cout << "TCP //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 17){
-            cout << "UDP //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 41){ //TO DISCUSS!!!
-            cout << "IPv6?? //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 50){
-            cout << "ESP //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 51){
-            cout << "AH //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 89){
-            cout << "OSPF //TO DO//\n";
-        }
-        else if (static_cast<int>(ipHeader.protocol) == 132){
-            cout << "SCTP //TO DO//\n";
-        }
-
-        //checksum
-        cout << "CHECKSUM: 0x" << hex << ipHeader.headerChecksum << "\n";
-
-        //source IP
-        uint8_t octet1 = (ipHeader.sourceIP >> 24) & 0xFF;
-        uint8_t octet2 = (ipHeader.sourceIP >> 16) & 0xFF;
-        uint8_t octet3 = (ipHeader.sourceIP >> 8) & 0xFF;
-        uint8_t octet4 = ipHeader.sourceIP & 0xFF;
-
-        cout << "SOURCE IP: " << (int)octet1 << "." << (int)octet2 << "." << (int)octet3 << "." << (int)octet4 << "\n";
-
-        //destination IP
-        octet1 = (ipHeader.destinationIP >> 24) & 0xFF;
-        octet2 = (ipHeader.destinationIP >> 16) & 0xFF;
-        octet3 = (ipHeader.destinationIP >> 8) & 0xFF;
-        octet4 = ipHeader.destinationIP & 0xFF;
-
-        cout << "DESTINATION IP: " << (int)octet1 << "." << (int)octet2 << "." << (int)octet3 << "." << (int)octet4 << "\n";
-
-        cout << dec;
-        //data
-        cout << "DATA: ";
-        cout << packetData.size() << "\n";
-//        for (size_t i = sizeof(EthernetHeader) + static_cast<unsigned int>(ihl); i < packetData.size(); ++i) {
-//            cout << i << "\n";
-//
-////            if ((i + 1) % 16 == 0)
-////                cout << "\n";
-//        }
 
     }
     else if (ethernetHeader.etherType == 34525){
@@ -285,4 +223,113 @@ void parseEthernet(vector<uint8_t> packetData) {
         cout << "I apologise but we're not network scientist we only know IPv4, IPv6, and ARP. Most likely this packet is corrupted so ye. Please refer to WireShark if you want a more professional tool :)\n";
     }
 
+}
+
+void parseIPv4(IPHeader ipHeader, vector<uint8_t> packetData) {
+    //version and internet header length (ihl)
+    uint8_t version = (ipHeader.versionIHL >> 4) & 0x0F;
+    cout << "VERSION: " << static_cast<unsigned int>(version) << "\n";
+    uint8_t ihl = (ipHeader.versionIHL & 0x0F) * 4;
+    cout << "INTERNET HEADER LENGTH(IHL): " << static_cast<unsigned int>(ihl) <<"\n";
+
+    //type of service and Differentiated Service Code Point(DSCP)
+    cout << "TYPE OF SERVICE: ";
+    uint8_t dscp = (ipHeader.typeOfService >> 2) & 0x3F;
+    if (static_cast<unsigned int>(dscp) == 0)
+        cout << "Default\n";
+    else if (static_cast<unsigned int>(dscp) == 46)
+        cout << "Expedited Forwarding\n";
+    else if (dscp == 0x08 or dscp == 0x0C or dscp == 0x10 or dscp == 0x10 or dscp == 0x1C or dscp == 0x20 or dscp == 0x28 or dscp == 0x2C or dscp == 0x30 or dscp == 0x38 or dscp == 0x3C or dscp == 0x40)
+        cout << "Assured Forwarding\n";
+    else cout << "Unknown\n";
+
+    //total length
+    ipHeader.totalLength = ntohs(ipHeader.totalLength);
+    cout << "TOTAL LENGTH: " << ipHeader.totalLength << " bytes\n";
+
+    //identification field
+    ipHeader.identification = ntohs(ipHeader.identification);
+    cout << "IDENTIFICATION: " << ipHeader.identification << "\n";
+
+    //flags and fragment offset
+    ipHeader.flagsFragmentOffset = ntohs(ipHeader.flagsFragmentOffset);
+    uint16_t flag = (ipHeader.flagsFragmentOffset & 0xE000) >> 13;
+    uint16_t fragmentOffset = ipHeader.flagsFragmentOffset & 0x1FFF;
+    bool isDFSet = (flag & 0x2) != 0;
+    bool isMFSet = (flag & 0x4) != 0;
+
+    cout << "FLAG: ";
+    if (isDFSet)
+        cout << "Don't Fragment (DF)\n";
+    else if (isMFSet)
+        cout << "More Fragments (MF)\n";
+    else if (flag == 0)
+        cout << "Reserved\n";
+    else cout << "Unknown\n";
+    cout << "FRAGMENT OFFSET: " << fragmentOffset << "\n";
+
+    //time to live
+    cout << "TIME TO LIVE: " << static_cast<int>(ipHeader.timeToLive) << "\n";
+
+    //protocol
+    cout << "PROTOCOL: ";
+    if(static_cast<int>(ipHeader.protocol) == 1){
+        cout << "ICMP //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 6){
+        cout << "TCP //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 17){
+        cout << "UDP //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 41){ //TO DISCUSS!!!
+        cout << "IPv6?? //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 50){
+        cout << "ESP //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 51){
+        cout << "AH //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 89){
+        cout << "OSPF //TO DO//\n";
+    }
+    else if (static_cast<int>(ipHeader.protocol) == 132){
+        cout << "SCTP //TO DO//\n";
+    }
+
+    //checksum
+    cout << "CHECKSUM: 0x" << hex << ipHeader.headerChecksum << "\n";
+
+    //source IP
+    ipHeader.sourceIP = ntohl(ipHeader.sourceIP);
+    uint8_t octet1 = (ipHeader.sourceIP >> 24) & 0xFF;
+    uint8_t octet2 = (ipHeader.sourceIP >> 16) & 0xFF;
+    uint8_t octet3 = (ipHeader.sourceIP >> 8) & 0xFF;
+    uint8_t octet4 = ipHeader.sourceIP & 0xFF;
+
+    cout << "SOURCE IP: "
+         << dec << static_cast<int>(octet1) << "." << static_cast<int>(octet2) << "."
+         << static_cast<int>(octet3) << "." << static_cast<int>(octet4) << "\n";
+
+    //destination IP
+    ipHeader.destinationIP = ntohl(ipHeader.destinationIP);
+    octet1 = (ipHeader.destinationIP >> 24) & 0xFF;
+    octet2 = (ipHeader.destinationIP >> 16) & 0xFF;
+    octet3 = (ipHeader.destinationIP >> 8) & 0xFF;
+    octet4 = ipHeader.destinationIP & 0xFF;
+
+    cout << "DESTINATION IP: "
+         << dec << static_cast<int>(octet1) << "." << static_cast<int>(octet2) << "."
+         << static_cast<int>(octet3) << "." << static_cast<int>(octet4) << "\n";
+
+    cout << dec;
+    //data printing
+    cout << "DATA: ";
+        for (size_t i = sizeof(EthernetHeader) + static_cast<unsigned int>(ihl); i < packetData.size(); ++i) {
+            cout << hex << packetData[i];
+
+            if ((i + 1) % 16 == 0)
+                cout << dec << "\n";
+        }
 }
